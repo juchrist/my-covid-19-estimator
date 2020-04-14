da4826bfc68367b1500277f4127c2cf738457137
juchrist <kingjuwon4christ@gmail.com>
2020-04-14 14:47:34 +0100

    8

--- src/estimator.php
+++ src/estimator.php
@@ -136,7 +136,7 @@ function assessmentThree(){
 
     $GLOBALS['casesForICUByRequestedTime'] = (int) (0.05 * $GLOBALS['infectionsByRequestedTime']);
     $GLOBALS['casesForVentilatorsByRequestedTime'] = (int)(0.02 * $GLOBALS['infectionsByRequestedTime']);
-    $GLOBALS['dollarsInFlight'] = intval(($GLOBALS['infectionsByRequestedTime'] * $GLOBALS['avgDailyIncomeInUSD'] * $GLOBALS['avgDailyIncomePopulation']) / $GLOBALS['estimatedPeriod']);
+    $GLOBALS['dollarsInFlight'] = intval(($GLOBALS['infectionsByRequestedTime'] * $GLOBALS['avgDailyIncomeInUSD'] * $GLOBALS['avgDailyIncomePopulation']));
 }
 
 
